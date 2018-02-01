346167b02d771b38ea5237121e389669d8f121f8
Christopher Luong <Christopher@luong-mac.local>
2018-02-01 17:40:08 -0600

    Initial Commit

---
+++ Swift Language Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Language Basics
+//
+//  Created by Christopher Luong on 2/1/18.
+//  Copyright © 2018 Christopher Luong. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
